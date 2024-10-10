6c4ba21eefc1112b9d74897e0dda3abf75e36b7e
ClauBaqueroRico <camabari@gmail.com>
2024-10-09 21:41:05 -0500

    Ajuste Inicio

--- inicio.py
+++ inicio.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import login as login
 
-st.header('Bienbenido :blue[porfavor ingrese sus credenciales]')
+st.header('Bienvenido :blue[porfavor ingrese sus credenciales]')
 login.generarLogin()
 if 'usuario' in st.session_state:
     st.subheader('Información página principal')
