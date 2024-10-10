b8384a6f337183917fac57350d498bcc85555ab8
ClauBaqueroRico <101302038+ClauBaqueroRico@users.noreply.github.com>
2024-10-10 20:34:54 +0000

    Logo_BV mejor constraste

--- inicio.py
+++ inicio.py
@@ -2,14 +2,21 @@ import streamlit as st
 import login as login
 
 # Configuración de la página
-st.set_page_config(page_title="Corrosion Interna", page_icon="📊", layout="wide")
+st.set_page_config(page_title="Corrosion Interna", page_icon="📶", layout="wide")
 
 # Crear columnas para los logos
 col1, col2, col3 = st.columns([1, 6, 1])  # Ajusta los anchos de las columnas como prefieras
 
 # Colocar el logo en la columna izquierda
 with col1:
-    st.image("LogoBV-White.png", width=150)  # Ajusta el tamaño según sea necesario
+    st.markdown(
+        """
+        <div style="background-color: #f0f0f0; padding: 10px;">
+            <img src="LogoBV-White.png" width="150">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 # Colocar el logo en la columna derecha
 with col3:
