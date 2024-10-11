9688e359585bae8e51f781ad2348fe891f602ab6
ClauBaqueroRico <101302038+ClauBaqueroRico@users.noreply.github.com>
2024-10-11 11:00:04 -0500

    Mundo

--- pages/BV_Corridas.py
+++ pages/BV_Corridas.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import pandas as pd
 from datetime import datetime
 import login
 
-# Configuración de la página: debe ser la primera llamada de Streamlit
+# Configuración de la página
 st.set_page_config(
     page_title="Informacion Corrosion Integral",
     page_icon="📊",
@@ -26,36 +25,46 @@ today = datetime.today().strftime("%Y")
 st.subheader('Servicios de Atención Integral de la Amenaza de Corrosión en Equipos Estáticos de la Infraestructura de Transporte de Hidrocarburos')
 
 # Contenedor principal
-with st.container():
-    st.title("Links de interés y tableros de control")
-    
-    # Sección de enlaces
-    sections = {
-        "Planeación y Seguimiento": [
-            ("Reporte Diario Operativo", "https://forms.office.com/Pages/ResponsePage.aspx?id=FNT6_6O2Mk-pvULSj8gR8dSPcBsCbEhJgdkPK01vntFURU9EMVg5V1UwWFpJRjA2NTJUTk5CSUVWNSQlQCN0PWcu"),
-            ("Tablero de Control Administrativo", "https://app.powerbi.com/view?r=eyJrIjoiNTk1MDIwMmMtYjEzZi00NDkwLTk2ZDItZjQyYjM1Mzc3NjEyIiwidCI6ImE2YjRmOTliLWQ1NzItNDFhYy05MDExLTRkMzAyNTBiYjkyYiIsImMiOjR9")
-        ],
-        "Corrosión Interna": [
-            ("Informe de Corrosión Interna", "http://example.com/corrosioninterna2"),
-            ("Informe de Corrosión Interna 2", "http://example.com/corrosioninterna2")
-        ],
-        "Corrosión Externa": [
-            ("Gestión de Corrosión", "https://app.bureauveritas.com.co/URPCv2/Account/Login"),
-            ("Informe de Corrosión Externa 2", "http://example.com/corrosioninterna2")
-        ],
-        "Analítica": [
-            ("Informe de Analítica Illis", "https://app.powerbi.com/view?r=eyJrIjoiZTgwMWE2MDYtZjEwNS00MWYwLWJkNDItZmRjYjg5NWJkMGQwIiwidCI6ImE2YjRmOTliLWQ1NzItNDFhYy05MDExLTRkMzAyNTBiYjkyYiIsImMiOjR9"),
-            ("Informe de Analítica 2", "http://example.com/analitica2")
-        ]
-    }
+st.title("Links de interés y tableros de control")
 
-    for title, links in sections.items():
-        with st.expander(title):
+# Sección de enlaces con dos columnas
+sections = {
+    "Planeación y Seguimiento": [
+        ("Reporte Diario Operativo", "https://forms.office.com/Pages/ResponsePage.aspx?id=FNT6_6O2Mk-pvULSj8gR8dSPcBsCbEhJgdkPK01vntFURU9EMVg5V1UwWFpJRjA2NTJUTk5CSUVWNSQlQCN0PWcu"),
+        ("Tablero de Control Administrativo", "https://app.powerbi.com/view?r=eyJrIjoiNTk1MDIwMmMtYjEzZi00NDkwLTk2ZDItZjQyYjM1Mzc3NjEyIiwidCI6ImE2YjRmOTliLWQ1NzItNDFhYy05MDExLTRkMzAyNTBiYjkyYiIsImMiOjR9")
+    ],
+    "Corrosión Interna": [
+        ("Informe de Corrosión Interna", "http://example.com/corrosioninterna2"),
+        ("Informe de Corrosión Interna 2", "http://example.com/corrosioninterna2")
+    ],
+    "Corrosión Externa": [
+        ("Gestión de Corrosión", "https://app.bureauveritas.com.co/URPCv2/Account/Login"),
+        ("Informe de Corrosión Externa 2", "http://example.com/corrosioninterna2")
+    ],
+    "Analítica": [
+        ("Informe de Analítica Illis", "https://app.powerbi.com/view?r=eyJrIjoiZTgwMWE2MDYtZjEwNS00MWYwLWJkNDItZmRjYjg5NWJkMGQwIiwidCI6ImE2YjRmOTliLWQ1NzItNDFhYy05MDExLTRkMzAyNTBiYjkyYiIsImMiOjR9"),
+        ("Informe de Analítica 2", "http://example.com/analitica2")
+    ]
+}
+
+# Dividir la interfaz en dos columnas
+col1, col2 = st.columns(2)
+
+for index, (title, links) in enumerate(sections.items()):
+    if index % 2 == 0:
+        with col1:
+            st.markdown(f"### {title}")
             for link_text, link_url in links:
-                st.markdown(f"[{link_text}]({link_url})")
+                st.markdown(f"- [{link_text}]({link_url})")
+    else:
+        with col2:
+            st.markdown(f"### {title}")
+            for link_text, link_url in links:
+                st.markdown(f"- [{link_text}]({link_url})")
 
-    # Sección de Estrategia y enlace a "Otros"
-    st.markdown("### Estrategia")
-    st.markdown("[![Mundo](https://path/to/world-logo.png)](http://example.com/otros) Otros")
+# Sección de Estrategia y enlace a "Otros"
+st.markdown("### Estrategia")
+st.markdown("[![data_thresholding](https://path/to/world-logo.png)](http://example.com/otros) Otros")
+
 
 
